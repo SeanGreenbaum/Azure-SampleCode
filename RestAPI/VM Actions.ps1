f53a57443371f390c8e4c621231fc6a5f7f20673
Sean Greenbaum <seangreenbaum@outlook.com>
2025-06-03 10:59:56 -0400

    update api version

--- RestAPI/VM Actions.ps1
+++ RestAPI/VM Actions.ps1
@@ -8,14 +8,14 @@ $headers = @{
 
 #List all VMs in a given subscription
 $subscriptionid = (Get-AzContext).Subscription.Id
-$apiVersion = "2024-07-01"
+$apiVersion = "2024-11-01"
 $uri = "https://management.azure.com/subscriptions/$subscriptionid/providers/Microsoft.Compute/virtualMachines?api-version=$apiVersion"
 $Result = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers
 $Result.value | Format-Table Name, location
 
 #List all VMs in a given Resource Group
 $resourcegroup = ""
-$apiVersion = "2024-07-01"
+$apiVersion = "2024-11-01"
 $subscriptionid = (Get-AzContext).Subscription.Id
 $uri = "https://management.azure.com/subscriptions/$subscriptionid/resourceGroups/$resourcegroup/providers/Microsoft.Compute/virtualMachines?api-version=$apiVersion"
 $Result = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers
@@ -25,7 +25,7 @@ $Result.value | Format-Table Name, location
 $subscriptionid = (Get-AzContext).Subscription.Id
 $ResourceGRoupName = ""
 $VMName = ""
-$apiversion = "2024-07-01"
+$apiversion = "2024-11-01"
 $uri = "https://management.azure.com/subscriptions/$SubscriptionID/resourceGroups/$ResourceGroupName/providers/Microsoft.Compute/virtualMachines/$VMName/start?api-version=$apiversion"
 Invoke-RestMethod -Method Post -Uri $uri -Headers $headers
 
@@ -33,7 +33,7 @@ Invoke-RestMethod -Method Post -Uri $uri -Headers $headers
 $subscriptionid = (Get-AzContext).Subscription.Id
 $ResourceGRoupName = ""
 $VMName = ""
-$apiversion = "2024-07-01"
+$apiversion = "2024-11-01"
 $uri = "https://management.azure.com/subscriptions/$SubscriptionID/resourceGroups/$ResourceGroupName/providers/Microsoft.Compute/virtualMachines/$VMName/deallocate?api-version=$apiversion"
 Invoke-RestMethod -Method Post -Uri $uri -Headers $headers
 
